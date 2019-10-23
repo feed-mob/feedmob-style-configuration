222487b83cc68f60ad5b3727c599fa6074b82858
Richard Hao <ithaoxilu@gmail.com>
2019-10-23 09:42:43 +0800

    Initial gem feedmob-style https://github.com/feed-mob/tracking_admin/issues/5878

---
+++ lib/feedmob/style.rb
@@ -0,0 +1,8 @@
+require "feedmob/style/version"
+
+module Feedmob
+  module Style
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/feedmob/style/version.rb
@@ -0,0 +1,5 @@
+module Feedmob
+  module Style
+    VERSION = "0.1.0"
+  end
+end
